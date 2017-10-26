1d4f3c3f01c2b1876a1a2e6792a66dde7a5086f4
Gilles de Hollander <gilles.de.hollander@gmail.com>
2017-10-26 18:20:53 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymp2rage",
-    version="0.1dev",
+    version="0.0.1dev",
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    author='Gilles de Hollander',
+    keywords='MRI high-resolution laminar',
+    install_requires=['numpy', 'nibabel', 'nilearn'],
 )
