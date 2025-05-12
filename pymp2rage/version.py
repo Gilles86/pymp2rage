76178f42ad860d1dd249adb30a81ae53d01692d2
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-05-12 20:12:36 +0200

    Bugfix

--- pymp2rage/version.py
+++ pymp2rage/version.py
@@ -20,7 +20,7 @@ __version__ = '.'.join(map(str, _ver))
 CLASSIFIERS = ["Development Status :: 3 - Alpha",
                "Environment :: Console",
                "Intended Audience :: Science/Research",
-               "License :: OSI Approved :: Apache License, 2.0"",
+               "License :: OSI Approved :: Apache License, 2.0",
                "Operating System :: OS Independent",
                "Programming Language :: Python",
                "Topic :: Scientific/Engineering"]
